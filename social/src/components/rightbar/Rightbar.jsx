aaf00537fc5f037b472aebe58bfc722195a79867
vasuve_altair <vasuve@altair.com>
2024-07-06 15:46:53 +0530

    rightbar commit

--- social/src/components/rightbar/Rightbar.jsx
+++ social/src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,60 @@ import './Rightbar.css'
 
 function Rightbar() {
   return (
-    <div className='rightbar'>Rightbar</div>
+    <div className='rightbar'>
+        <div className="rightBarWrapper"> 
+            <div className="birthDayContainer">
+            <img className="birthdayImage" src="assets/posts/gift.png" alt="birthday icon" />
+            <span className="birthdayText"> <b>Vineela </b>and <b>3 other friends have birthday Today ! </b> </span>
+            </div>
+            <img src="assets/posts/ad.png" alt="advertisement" className="rightbarAD" />
+            <h4 className="rightbarTitle"> Online Friends </h4>
+            <ul className="rightBarFriendList">
+             <li className="rightBarFriend">
+                <div className="rightBarProfileImageContainer">
+                   <img src="assets/person/3.jpeg" alt="" className="rightBarProfileImage" />
+                   <span className='rightBarOnline'></span>
+                </div>
+                   <span className="rightBarUserName">Johny Depp</span>
+              </li>
+              <li className="rightBarFriend">
+                <div className="rightBarProfileImageContainer">
+                   <img src="assets/person/3.jpeg" alt="" className="rightBarProfileImage" />
+                   <span className='rightBarOnline'></span>
+                </div>
+                   <span className="rightBarUserName">Johny Depp</span>
+              </li>
+              <li className="rightBarFriend">
+                <div className="rightBarProfileImageContainer">
+                   <img src="assets/person/3.jpeg" alt="" className="rightBarProfileImage" />
+                   <span className='rightBarOnline'></span>
+                </div>
+                   <span className="rightBarUserName">Johny Depp</span>
+              </li>
+              <li className="rightBarFriend">
+                <div className="rightBarProfileImageContainer">
+                   <img src="assets/person/3.jpeg" alt="" className="rightBarProfileImage" />
+                   <span className='rightBarOnline'></span>
+                </div>
+                   <span className="rightBarUserName">Johny Depp</span>
+              </li>
+              <li className="rightBarFriend">
+                <div className="rightBarProfileImageContainer">
+                   <img src="assets/person/3.jpeg" alt="" className="rightBarProfileImage" />
+                   <span className='rightBarOnline'></span>
+                </div>
+                   <span className="rightBarUserName">Johny Depp</span>
+              </li>
+              <li className="rightBarFriend">
+                <div className="rightBarProfileImageContainer">
+                   <img src="assets/person/3.jpeg" alt="" className="rightBarProfileImage" />
+                   <span className='rightBarOnline'></span>
+                </div>
+                   <span className="rightBarUserName">Johny Depp</span>
+              </li>
+              </ul>
+       </div> 
+    </div>
   )
 }
 
